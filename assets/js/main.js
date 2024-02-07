a096822552133a325480a95d20af5cc63c619918
Byron Rode <byronrode@gmail.com>
2024-02-07 18:56:31 +0200

    Mobile tweaks and fixes for menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,22 @@ const currentURL = window.location.href;
 if (!currentURL.includes('localhost')) {
   trackLinks();
 }
+
+// Handle Mobile Navigation
+const navMenu = document.querySelector('.nav-menu-button');
+const closeNavMenu = document.querySelector('.close-nav-menu');
+if (navMenu) {
+  const mainNav = document.querySelector('.main-navigation-mobile');
+  navMenu.addEventListener('click', (ev) => {
+    if(mainNav){
+      mainNav.classList.toggle('hidden');
+    }
+    if(closeNavMenu){
+      closeNavMenu.addEventListener('click', (ev) => {
+        mainNav.classList.toggle('hidden');
+      })
+    }
+
+  })
+}
+

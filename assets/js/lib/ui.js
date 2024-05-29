734084a0130948d31dc3d953ef70efc0abb4173c
Byron Rode <byronrode@gmail.com>
2024-05-29 11:22:37 +0200

    Add glow class highlighter

--- assets/js/lib/ui.js
+++ assets/js/lib/ui.js
@@ -1,37 +1,42 @@
 const mouseTrackingGlow = () => {
-    document.addEventListener('DOMContentLoaded', () => {
-        const mainElement = document.getElementById('main-element');
-        const navigationElement = document.querySelector('.main-navigation');
-        const workHistory = document.querySelector('.work-history');
+  document.addEventListener('DOMContentLoaded', () => {
+    const mainElement = document.getElementById('main-element');
+    const navigationElement = document.querySelector('.main-navigation');
+    const workHistory = document.querySelector('.work-history');
+    const glow = document.querySelector('.glow');
 
+    const handleMouseMove = (e) => {
+      const rect = mainElement.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
 
-        const handleMouseMove = (e) => {
+      mainElement.style.setProperty('--x', `${x}px`);
+      mainElement.style.setProperty('--y', `${y}px`);
 
-            const rect = mainElement.getBoundingClientRect();
-            const x = e.clientX - rect.left;
-            const y = e.clientY - rect.top;
+      // Navigation
+      const navRect = navigationElement.getBoundingClientRect();
+      const navX = x - (navRect.left - rect.left);
+      const navY = y - (navRect.top - rect.top);
+      navigationElement.style.setProperty('--x', `${navX}px`);
+      navigationElement.style.setProperty('--y', `${navY}px`);
 
-            mainElement.style.setProperty('--x', `${x}px`);
-            mainElement.style.setProperty('--y', `${y}px`);
+      // Work History
+      const workRect = workHistory.getBoundingClientRect();
+      const workX = x - (workRect.left - rect.left);
+      const workY = y - (workRect.top - rect.top);
+      workHistory.style.setProperty('--x', `${workX}px`);
+      workHistory.style.setProperty('--y', `${workY}px`);
 
-            // Navigation
-            const navRect = navigationElement.getBoundingClientRect();
-            const navX = x - (navRect.left - rect.left);
-            const navY = y - (navRect.top - rect.top);
-            navigationElement.style.setProperty('--x', `${navX}px`);
-            navigationElement.style.setProperty('--y', `${navY}px`);
+      // Glow
+      const glowRect = glow.getBoundingClientRect();
+      const glowX = x - (glowRect.left - rect.left);
+      const glowY = y - (glowRect.top - rect.top);
+      glow.style.setProperty('--x', `${glowX}px`);
+      glow.style.setProperty('--y', `${glowY}px`);
+    };
 
-            // Work History
-            const workRect = workHistory.getBoundingClientRect();
-            const workX = x - (workRect.left - rect.left);
-            const workY = y - (workRect.top - rect.top);
-            workHistory.style.setProperty('--x', `${workX}px`);
-            workHistory.style.setProperty('--y', `${workY}px`);
-
-        };
-
-        mainElement.addEventListener('mousemove', handleMouseMove);
-    });
-}
+    mainElement.addEventListener('mousemove', handleMouseMove);
+  });
+};
 
 module.exports = mouseTrackingGlow;
